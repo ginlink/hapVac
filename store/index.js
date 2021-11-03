3b43b2c6e1cb624610e62b33420e8655e96c3b06
蒋金 <ginlink@163.com>
2021-11-04 01:21:34 +0800

    init

---
+++ store/index.js
@@ -0,0 +1,53 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export const store = new Vuex.Store({
+	state: {
+		isLoadStartPage: false,
+		centerMenu: null,
+		appNum: undefined,
+		notices: null,
+		version: undefined,
+		validCode: null,
+	},
+	mutations: {
+		updateIsLoadStartPage(store, payload) {
+			store.isLoadStartPage = payload
+		},
+		updateCenterMenu(store, payload) {
+			store.centerMenu = payload
+		},
+		updateAppNum(store, payload) {
+			store.appNum = payload
+		},
+		updateNotices(store, payload) {
+			store.notices = payload
+		},
+		updateVersion(store, payload) {
+			store.version = payload
+		},
+		updateValidCode(store, payload) {
+			store.validCode = payload
+		}
+	},
+	getters: {
+		centerMenu: state => {
+			return state.centerMenu
+		},
+		appNum: state => {
+			return state.appNum
+		},
+		notices: state => {
+			return state.notices
+		}
+		,
+		version: state => {
+			return state.version
+		},
+		validCode: state => {
+			return state.validCode
+		}
+	}
+})
