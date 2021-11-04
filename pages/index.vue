e558060f2928efb5e27e6b5e8bc77f885267450e
蒋金 <ginlink@163.com>
2021-11-04 18:27:47 +0800

    feat: 测试微信登录&获取用户信息

--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,9 @@
     <console v-else-if="startPage.indexOf('pages/console/console') != -1 && isValid" />
     <view v-else> </view>
 
+    <u-button @click="loginWechat">微信登录</u-button>
+    <u-button @click="getWechatUserInfo">获取用户信息</u-button>
+
     <view class="popup-wrapper">
       <u-popup v-model="show" mode="center" border-radius="14" width="80%" :mask-close-able="false">
         <view class="popup-content">
@@ -96,14 +99,13 @@ export default {
         }
 
         const localValidCode = uni.getStorageSync('LOCAL_VALID_CODE')
-        const shouldValidCode = this.shouldValidCode
 
-        console.log('[](localValidCode,shouldValidCode,correctCode):', localValidCode, shouldValidCode, correctCode)
+        console.log('[](localValidCode,shouldValidCode,correctCode):', localValidCode, correctCode)
 
-        const isValid = localValidCode ? localValidCode !== correctCode : shouldValidCode !== correctCode
+        const isValid = localValidCode === correctCode
 
-        this.isValid = !isValid
-        return isValid
+        this.isValid = isValid
+        return !isValid
       },
       set(val) {
         console.log('[](val):', val)
@@ -124,6 +126,63 @@ export default {
     },
   },
   methods: {
+    getWechatUserInfo() {
+      uni.getUserProfile({
+        desc: '绑定用户',
+        success: function (infoRes) {
+          console.log('用户昵称为：' + infoRes.encryptedData)
+
+          const encryptedData = infoRes.encryptedData
+          const iv = infoRes.iv
+
+          const token1 =
+            'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Niwib3Blbl9pZCI6Im9SSTVrNWVTamxSTWNyN3hPTnc5eFptU3VGeDgiLCJzZXNzaW9uX2tleSI6Imw2MXRTaml5Q09lUmpPV0Npc2pjNEE9PSIsIm5pY2tfbmFtZSI6bnVsbCwiYXZhdGFyX3VybCI6bnVsbCwiaWF0IjoxNjM2MDE4Mzc4LCJleHAiOjE2MzYwMzI3Nzh9.vs64KToEh-NaRMMECj2pYSnmDbHlsrBNNlS55_n41JU'
+          const token2 =
+            'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Niwib3Blbl9pZCI6Im9SSTVrNWVTamxSTWNyN3hPTnc5eFptU3VGeDgiLCJzZXNzaW9uX2tleSI6InVyLzE0MzcwNloybUFuaXlJMGl5M1E9PSIsIm5pY2tfbmFtZSI6IuWIh-aNouWIsOWkp-WktCIsImF2YXRhcl91cmwiOiJodHRwczovL3RoaXJkd3gucWxvZ28uY24vbW1vcGVuL3ZpXzMyL1EwajRUd0dUZlRLbHB1WHoxNDZVOFFMa0ZxUmdOdGJjcjNDa0dkSXVrMHluWUdQajBIS0pVQkJDbVN2ODhpY28ySkg1Z0c2ZHlhZDJWWm9ZaWFiamliVjFnLzEzMiIsImlhdCI6MTYzNjAyMDk3MywiZXhwIjoxNjM2MDM1MzczfQ.ptA0J9_Ttuw_WJuq2um9lLZdAOekLftOZ9AT4D6HsSg'
+          // config.headers.Authorization = `Bearer ${token}`;
+          uni.request({
+            url: 'http://192.168.3.42:3040/api/user/getUserInfo/', //仅为示例，并非真实接口地址。
+            method: 'POST',
+            data: {
+              encryptedData,
+              iv,
+            },
+            header: {
+              'custom-header': 'hello', //自定义请求头信息
+              // Authorization: `Bearer ${token1}`,
+              Authorization: `Bearer ${token2}`,
+            },
+            success: (res) => {
+              console.log(res.data)
+            },
+          })
+        },
+      })
+    },
+    loginWechat() {
+      uni.login({
+        provider: 'weixin',
+        success: function (loginRes) {
+          console.log(loginRes.authResult)
+
+          const code = loginRes.code
+
+          uni.request({
+            url: 'http://192.168.3.42:3040/api/auth/login', //仅为示例，并非真实接口地址。
+            method: 'POST',
+            data: {
+              code,
+            },
+            header: {
+              'custom-header': 'hello', //自定义请求头信息
+            },
+            success: (res) => {
+              console.log(res.data)
+            },
+          })
+        },
+      })
+    },
     sure() {
       console.log('[](我知道了):')
       // 更新本地版本
